f7efb0f9bbdc18d35e7a9afa8d3a56574b5b9bc7
Barrett Anderson <barrett1614@gmail.com>
2024-03-31 10:13:17 -0500

    Initial

---
+++ Commit.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const simpleGit = require("simple-git");
+const git = simpleGit();
+
+const min = 1;
+const max = 2;
+const date = new Date();
+const maxCommit = Math.floor(Math.random() * (max - min + 1)) + min;
+
+async function checkCommit(fileName) {
+  try {
+    const log = await git.log({ file: fileName });
+    return log.all.length > 0;
+  } catch (error) {
+    console.error("Error checking commit:", error);
+    return false;
+  }
+}
+
+(async () => {
+  for (let i = 0; i <= maxCommit; i++) {
+    const fileName = `./TXT-Files/file${i}.txt`;
+    const txtOfFile = `This is file number ${i}. Date and Time: ${date}.`;
+
+    fs.writeFileSync(fileName, `${txtOfFile}`);
+
+    await git.add(fileName);
+    await git.commit(`Commit number ${i}`);
+    console.log(`Pushed ${fileName} to given repo.`);
+
+    const isCommitted = await checkCommit(fileName);
+    console.log(`${fileName} committed? ${isCommitted}`);
+  }
+
+  // Push all the files to remote repository
+  await git.push("origin", "main");
+})();
