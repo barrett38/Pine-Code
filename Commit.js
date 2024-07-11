515af2ca30e839c48eec61ee39f793b86c0ca512
Barrett Anderson <barrett1614@gmail.com>
2024-07-10 22:53:25 -0500

    Changed number

--- Commit.js
+++ Commit.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 const simpleGit = require("simple-git");
 const git = simpleGit();
 
-const min = 7;
-const max = 8;
+const min = 2;
+const max = 3;
 const date = new Date();
 const maxCommit = Math.floor(Math.random() * (max - min + 1)) + min;
 
